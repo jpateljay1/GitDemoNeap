868d53843bb46543bdf3793cd584839ec1ed8f95
jaygpatel <jpatel.jay1@gmail.com>
2025-12-14 00:49:15 +0530

    third commit by jay g patel

---
+++ testCases/test_home.py
@@ -0,0 +1,7 @@
+import time
+
+from Utilities.ReadProperties import ReadConfig
+from pageObject.Landing_page import Landing_page
+import pytest
+from Utilities.BaseClass import BaseClass
+import logging
